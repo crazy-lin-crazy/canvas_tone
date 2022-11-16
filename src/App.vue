f9c8a7eb1e230433dd9ddacea8892c6edbaea203
zy <you@example.com>
2022-11-16 17:04:27 +0100

    feat:up partially

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="app">
-        <div class="canvas">
-            <canvas ></canvas>
+        <div class="canvas-container">
+            <canvas id="canvas" ref="canvas">
+                Oops! Canvas not supported in this browser
+            </canvas>
         </div>
     </div>
 </template>
@@ -10,21 +12,74 @@
 
 export default {
     name: 'App',
-    methods: {
-        paint(){
+    data(){
+        let data = {
+            isDown:false,
+            canvasX:0,
+            canvasY:0,
+        }
+        return {data}
+    },
+    mounted(){
+        let me = this
+        // init
 
+    },
+    methods: {
+        init(){
+
+        },
+        start(e){
+            let me = this
+            let data = me.data
+            data.isDown = true;
+            context.beginPath();
+            data.canvasX = e.pageX - selectCanvas.offsetLeft;
+            data.canvasY = e.pageY - selectCanvas.offsetTop;
+            context.moveTo(canvasX, canvasY);
+        },
+        draw(e){
+            let me = this
+            let data = me.data
+            if(data.isDown) {
+                data.canvasX = e.pageX - selectCanvas.offsetLeft;
+                data.canvasY = e.pageY - selectCanvas.offsetTop;
+                context.lineTo(data.canvasX, data.canvasY);
+                context.strokeStyle = selectedColor.value;
+                context.stroke();
+            }
+        },
+        stop(e){
+            let me = this
+            let data = me.data
+            data.isDown = false;
+            context.closePath();
+        },
+        changeStroke(e,obj){
+            let me = this
+            let data = me.data
+            if(e.target.dataset.stroke){
+                let parent = e.target.parentElement.children;
+                for(let i in parent){
+                    if(parent.hasOwnProperty(i)){
+                        if(e.target.className === parent[i].className){
+                            e.target.style.border = "2px dotted";
+                        }else{
+                            parent[i].style.border = "none";
+                        }
+                    }
+                }
+                context.lineWidth = e.target.dataset.stroke;
+            }
         }
     }
 }
 </script>
 
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+<style lang="less">
+#canvas{
+    width:400px;
+    height: 400px;
 }
+
 </style>
